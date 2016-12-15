15665e939c0a9af8ed9815b4ebed84c58b7929ec
CaSSoSSiSSe <yoyo250998@gmail.com>
2016-12-15 17:23:19 +0100

    Add files via upload

---
+++ historique.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+void afficher_historique();
+void trier_historique();
+void afficher_tri();
+void gestionHistorique(int argc, char *argv[]);
