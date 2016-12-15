de4f57a73f80d29408cf979f5a6b76f91604b706
Thomas Le Saux <toto3336@hotmail.fr>
2016-12-15 22:29:36 +0100

    Update dynamique.c

--- dynamique.c
+++ dynamique.c
@@ -1,10 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <termios.h>
-#include <unistd.h>
-#include <signal.h>
-#include "conio.h"
+#include "dynamique.h"
 
 void Afficher_heure ()
 {
