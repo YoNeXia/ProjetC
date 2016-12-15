5e5179ccbe75ae9903b00dfdbe8ef9ecaf696cb3
Thomas Le Saux <toto3336@hotmail.fr>
2016-12-15 22:29:33 +0100

    Create dynamique.h

---
+++ dynamique.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <termios.h>
+#include <unistd.h>
+#include <signal.h>
+#include "conio.h"
+
+void Afficher_heure ();
+void Actualisation_heure ();
